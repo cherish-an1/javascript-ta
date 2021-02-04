b2e77823eec4b0e95becdbc151dcde6aeccb2a93
Anozie Ngozi <eanozie98@gmail.com>
2021-02-04 23:02:31 +0100

    added palindrome function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 // complete the function
-function palindrom(str) {
+function palindrom(string) {
   // code goes here
+  var re = /[^A-Za-z0-9]/g;
+  string = string.toLowerCase().replace(re, '');
+  const len = string.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+
+    // check if first and last string are same
+    if (string[i] !== string[len - 1 - i]) {
+        return false;
+    }
+}
+return true;
 }
 
 function solution(arg) {
